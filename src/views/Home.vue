e745569ba4281c01d785553958aad85940c1e3ba
Pierre SAID <said.pierre.emler@gmail.com>
2020-05-09 23:22:17 +0200

    adjust margin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-10">
+  <div class="mt-8">
     <v-expansion-panels v-model="panel" focusable multiple accordion flat>
       <v-expansion-panel
         v-for="(section, index) in sections"
