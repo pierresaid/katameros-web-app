1f0e69db19afa93504ec681a62f00cc0423108a2
Pierre Said <said.pierre.emler@gmail.com>
2021-08-10 23:45:59 +0200

    Add pwa description

--- vue.config.js
+++ vue.config.js
@@ -6,32 +6,34 @@ module.exports = {
     themeColor: "#FFC107",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
-    description: "The Coptic lectionary. Church readings for all Week Days and Feasts.",
-    scope: "/",
-    screenshots: [
-      {
-        src: "img/katameros-screen-1.png",
-        sizes: "586x1189",
-        type: "image/png",
-      },
-      {
-        src: "img/katameros-screen-text.png",
-        sizes: "586x1189",
-        type: "image/png",
-      },
-      {
-        src: "img/katameros-screen-dark.png",
-        sizes: "586x1189",
-        type: "image/png",
-      },
-      {
-        src: "img/katameros-screen-menu.png",
-        sizes: "586x1189",
-        type: "image/png",
-      },
-    ],
-    categories: ["religion", "books"],
-    start_url: "https://katameros.app",
+    manifestOptions: {
+      scope: "/",
+      description: "The Coptic lectionary. Church readings for all Week Days and Feasts.",
+      screenshots: [
+        {
+          src: "img/katameros-screen-1.png",
+          sizes: "586x1189",
+          type: "image/png",
+        },
+        {
+          src: "img/katameros-screen-text.png",
+          sizes: "586x1189",
+          type: "image/png",
+        },
+        {
+          src: "img/katameros-screen-dark.png",
+          sizes: "586x1189",
+          type: "image/png",
+        },
+        {
+          src: "img/katameros-screen-menu.png",
+          sizes: "586x1189",
+          type: "image/png",
+        },
+      ],
+      categories: ["religion", "books"],
+      start_url: "https://katameros.app",
+    },
     iconPaths: {
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
