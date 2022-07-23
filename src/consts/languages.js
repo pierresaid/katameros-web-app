21a46e6d9a1b3dd146546ab3e74f4b281c5cbf18
Pierre Said <said.pierre.emler@gmail.com>
2022-07-23 23:39:41 +0200

    add italiano

--- src/consts/languages.js
+++ src/consts/languages.js
@@ -2,6 +2,7 @@ const LANGUAGES = {
   french: { id: 1, code: "fr", name: "Français" },
   english: { id: 2, code: "en", name: "English" },
   arabic: { id: 3, code: "ar", name: "عربي", rtl: true },
+  italian: { id: 4, code: "it", name: "Italiano" },
 };
 
 export default LANGUAGES;
