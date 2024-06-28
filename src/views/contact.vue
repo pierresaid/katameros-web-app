11de58aaec48a4fe4f26dc00e5c53f42b7666a96
Pierre Said <said.pierre.emler@gmail.com>
2024-06-28 22:32:15 +0200

    msg

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,8 +1,8 @@
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import { Form, Field } from 'vee-validate';
 import { useNotif } from '../store/notif';
+import { useI18n } from 'vue-i18n';
 
 const name = ref('');
 const email = ref('');
@@ -15,6 +15,7 @@ function encode(data: any) {
         .join("&")
 }
 
+const { t } = useI18n();
 async function onsubmit() {
     loading.value = true;
     await fetch("/", {
@@ -31,7 +32,7 @@ async function onsubmit() {
     email.value = '';
     message.value = '';
     loading.value = false
-    useNotif().success("Message sent.");
+    useNotif().success(t('MessageSent'));
 }
 </script>
 
@@ -40,7 +41,7 @@ async function onsubmit() {
         <h2>
             Contact
         </h2>
-        <Form v-slot="{ handleSubmit, errors }">
+        <Form v-slot="{ handleSubmit }">
             <Field v-slot="{ field, errorMessage }" name="name" rules="required">
                 <v-text-field v-model="name" :error-messages="errorMessage" autocomplete="name" v-bind="field"
                     name="name" label="Nom" required />
@@ -52,7 +53,8 @@ async function onsubmit() {
             <Field v-slot="{ errorMessage, field }" name="message" rules="required|min:5">
                 <v-textarea v-model="message" v-bind="field" :error-messages="errorMessage" label="Message" />
             </Field>
-            <v-btn @click.prevent="handleSubmit(onsubmit)" type="submit" color="primary" class="mr-4" :loading="loading">
+            <v-btn @click.prevent="handleSubmit(onsubmit)" type="submit" color="primary" class="mr-4"
+                :loading="loading">
                 Envoyer
             </v-btn>
         </Form>
