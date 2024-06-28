72070df7fafb71942244380b7eddcc3ff1384572
Pierre Said <said.pierre.emler@gmail.com>
2024-06-28 22:02:58 +0200

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5liclclm7cg"
+    "revision": "0.4ht7h7v3hro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
