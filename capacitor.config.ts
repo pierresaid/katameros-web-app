d252ffa07ed41b49e79fd4cfd93c1d29ed914f1b
Pierre SAID <said.pierre.emler@gmail.com>
2024-07-06 20:19:03 +0200

    Add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dcoanbamarc.katamerosmobile',
+  appName: 'Katameros',
+  webDir: 'dist',
+  backgroundColor: '#121212',
+};
+
+export default config;
