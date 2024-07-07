dc642165f277915557dbf9a9fc1194479ac4b4c7
Pierre SAID <said.pierre.emler@gmail.com>
2024-07-07 21:31:44 +0200

    chore: Update zoom range and button styling in zoom-picker.vue

--- src/components/zoom-picker.vue
+++ src/components/zoom-picker.vue
@@ -19,8 +19,8 @@ function dec() {
 <template>
     <div style="width: 100%;">
         <div class="font-weight-bold my-2">{{ $t('zoom') }}</div>
-        <v-slider v-model="menu.zoom" step="0.1" min="0.5" max="2.5" thumb-label="always" thumb-size="24" />
-        <v-btn-group variant="tonal" class="picker-toggle" style="margin-top: -20px;">
+        <v-slider v-model="menu.zoom" step="0.1" min="0.5" max="2.5"/>
+        <v-btn-group  density="compact" variant="tonal" class="picker-toggle" style="margin-top: -25px;">
             <v-btn size="x-small" style="padding: 0;" icon="mdi-magnify-minus" @click="dec" :aria-label="$t('aria.zoomIn')"/>
             <v-btn size="x-small" style="padding: 0;" icon="mdi-magnify-plus" @click="inc" :aria-label="$t('aria.zoomOut')"/>
         </v-btn-group>
